aa74e17fe4addd32ef9c35c8f507759f2295ad8d
Walter Barbagallo <turbometalskater@gmail.com>
2014-09-22 01:15:49 +0200

    added FOS js routing bundle

--- php/src/LinCC/TablesBundle/Resources/public/js/script.js
+++ php/src/LinCC/TablesBundle/Resources/public/js/script.js
@@ -1,3 +1,34 @@
+function Table(el) {
+    this.el = el;
+}
+
+Table.prototype.columnCount = function()
+{
+    return this.el.find('thead th').length;
+}
+
+Table.prototype.rowCount = function()
+{
+    console.log(this.el);
+    return this.el.find('tbody tr').length;
+}
+
+Table.prototype.addRows = function(row)
+{
+    var newEl = $('<tr><td>asd</td></tr>');
+    var res = Routing.generate('display_table', {name:'asd'});
+    //this.el.find('tbody').append(hincluderEl);
+    console.log(res);
+    return true;
+}
+
+var aTable;
 $(document).ready(function(){
-    alert('hello');
+    aTable = new Table($('table')); 
+});
+
+$(window).scroll(function() {
+    if($(window).scrollTop() + $(window).height() === $(document).height()) {
+        aTable.addRows();
+    }
 });
